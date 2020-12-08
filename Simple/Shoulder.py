a1796e3eedb615e63de286a1ba23a99b1e1521fb
Simon Budker <sbudker@gmail.com>
2020-12-07 23:58:17 -0800

    try this stepper

--- Simple/Shoulder.py
+++ Simple/Shoulder.py
@@ -1,71 +1,77 @@
 import pigpio
 from time import sleep
+import curses
 from collections import deque
 
-
-
 fullStepSequence = (
-  (1, 0, 0, 0),
-  (0, 1, 0, 0),
-  (0, 0, 1, 0),
-  (0, 0, 0, 1)
+    (1, 0, 0, 0),
+    (0, 1, 0, 0),
+    (0, 0, 1, 0),
+    (0, 0, 0, 1)
 )
-
-
 
 halfStepSequence = (
-  (1, 0, 0, 0),
-  (1, 1, 0, 0),
-  (0, 1, 0, 0),
-  (0, 1, 1, 0),
-  (0, 0, 1, 0),
-  (0, 0, 1, 1),
-  (0, 0, 0, 1),
-  (1, 0, 0, 1)
+    (1, 0, 0, 0),
+    (1, 1, 0, 0),
+    (0, 1, 0, 0),
+    (0, 1, 1, 0),
+    (0, 0, 1, 0),
+    (0, 0, 1, 1),
+    (0, 0, 0, 1),
+    (1, 0, 0, 1)
 )
-
 
 
 class StepperMotor:
 
-  def __init__(self, pi, pin1, pin2, pin3, pin4, sequence = halfStepSequence, delayAfterStep = 0.0025):
-    if not isinstance(pi, pigpio.pi):
-      raise TypeError("Is not pigpio.pi instance.")
-    pi.set_mode(pin1, pigpio.OUTPUT)
-    pi.set_mode(pin2, pigpio.OUTPUT)
-    pi.set_mode(pin3, pigpio.OUTPUT)
-    pi.set_mode(pin4, pigpio.OUTPUT)
-    self.pin1 = pin1
-    self.pin2 = pin2
-    self.pin3 = pin3
-    self.pin4 = pin4
-    self.pi = pi
-    self.delayAfterStep = delayAfterStep
-    self.deque = deque(sequence)
+    def __init__(self, pi, pin1, pin2, pin3, pin4, sequence=halfStepSequence, delayAfterStep=0.0025):
+        if not isinstance(pi, pigpio.pi):
+            raise TypeError("Is not pigpio.pi instance.")
+        pi.set_mode(pin1, pigpio.OUTPUT)
+        pi.set_mode(pin2, pigpio.OUTPUT)
+        pi.set_mode(pin3, pigpio.OUTPUT)
+        pi.set_mode(pin4, pigpio.OUTPUT)
+        self.pin1 = pin1
+        self.pin2 = pin2
+        self.pin3 = pin3
+        self.pin4 = pin4
+        self.pi = pi
+        self.delayAfterStep = delayAfterStep
+        self.deque = deque(sequence)
 
+    def doСounterclockwiseStep(self):
+        self.deque.rotate(-1)
+        self.doStepAndDelay(self.deque[0])
 
+    def doСlockwiseStep(self):
+        self.deque.rotate(1)
+        self.doStepAndDelay(self.deque[0])
 
-  def doСounterclockwiseStep(self):
-    self.deque.rotate(-1)
-    self.doStepAndDelay(self.deque[0])
+    def doStepAndDelay(self, step):
+        self.pi.write(self.pin1, step[0])
+        self.pi.write(self.pin2, step[1])
+        self.pi.write(self.pin3, step[2])
+        self.pi.write(self.pin4, step[3])
+        sleep(self.delayAfterStep)
 
-
-
-  def doСlockwiseStep(self):
-    self.deque.rotate(1)
-    self.doStepAndDelay(self.deque[0])
-
-
-
-  def doStepAndDelay(self, step):
-    self.pi.write(self.pin1, step[0])
-    self.pi.write(self.pin2, step[1])
-    self.pi.write(self.pin3, step[2])
-    self.pi.write(self.pin4, step[3])
-    sleep(self.delayAfterStep)
 
 if __name__ == '__main__':
     pi = pigpio.pi()
-    motor = StepperMotor(pi, 7, 11, 13, 15)
-    for i in range(2048):
-        motor.doСlockwiseStep()
+    stdscr = curses.initscr()
+    stdscr.keypad(True)
+    curses.cbreak()
+    curses.noecho()
+
+    try:
+        motor = StepperMotor(pi, 6, 13, 19, 26)
+        while True:
+            key = stdscr.getkey()
+            if key == "KEY_LEFT":
+                motor.doСlockwiseStep()
+            elif key == "KEY_RIGHT":
+                motor.doСounterclockwiseStep()
+    except Exception as e:
+        print(e)
+    finally:
+        curses.endwin()
+        pi.stop()
